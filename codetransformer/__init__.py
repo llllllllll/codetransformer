fe49f12895ad0a389a6bf8e1a56c0f1a3ffb09a3
llllllllll <joe@quantopian.com>
2015-03-02 00:20:21 -0500

    DEV: Adds setup.py

--- codetransformer/__init__.py
+++ codetransformer/__init__.py
@@ -1,6 +1,8 @@
 from codetransformer.base import CodeTransformer, Instruction
 from CodeTransformer.util import with_code_transformation
 
+__version__ = '0.1.0'
+
 __all__ = [
     'CodeTransformer',
     'Instruction',
---
+++ setup.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+from distutils.core import setup
+import sys
+
+long_description = ''
+
+if 'upload' in sys.argv:
+    with open('README.rst') as f:
+        long_description = f.read()
+
+setup(
+    name='codetransformer',
+    version='0.1.0',
+    description='Python code object transformers',
+    author='Joe Jevnik',
+    author_email='joejev@gmail.com',
+    packages=[
+        'codetransformer',
+    ],
+    long_description=long_description,
+    license='GPL-2',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: Implementation :: CPython',
+        'Operating System :: POSIX',
+        'Topic :: Software Development :: Pre-processors',
+    ],
+    url='https://github.com/llllllllll/codetransformer',
+)
