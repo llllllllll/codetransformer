537cfe7a8ded30ae241b1a44bb94213d718bb257
Scott Sanderson <scoutoss@gmail.com>
2015-06-23 02:27:07 -0400

    ENH: Pattern-matched exceptions
    Allows usage of arbitrary expressions in the argument to an `except` block.

---
+++ codetransformer/transformers/patterns.py
@@ -0,0 +1,102 @@
+from sys import exc_info
+
+from dis import (
+    cmp_op,
+    dis,
+)
+from enum import (
+    IntEnum,
+    unique,
+)
+
+
+from codetransformer.core import CodeTransformer
+from codetransformer.instructions import (
+    ROT_TWO,
+    POP_TOP,
+    CALL_FUNCTION,
+)
+
+
+@unique
+class Comparisons(IntEnum):
+    LT = 0
+    LE = 1
+    EQ = 2
+    NE = 3
+    GT = 4
+    GE = 5
+    IN = 6
+    NOT_IN = 7
+    IS = 8
+    IS_NOT = 9
+    EXCEPTION_MATCH = 10
+
+
+def match(match_expr, exc_info):
+    """
+    Called to determine whether or not an except block should be matched.
+
+    True -> enter except block
+    False -> don't enter except block
+    """
+    type_, value, tb = exc_info
+
+    # Emulate standard behavior when match_expr is an exception subclass.
+    if isinstance(match_expr, type) and issubclass(match_expr, BaseException):
+        return issubclass(type_, match_expr)
+
+    # Match on type and args when match_expr is an exception instance.
+    return (
+        issubclass(type_, type(match_expr)) and match_expr.args == value.args
+    )
+
+
+class pattern_matched_exceptions(CodeTransformer):
+    """
+    Allows usage of arbitrary expressions and matching functions in `except` blocks.
+
+    When an exception is raised in an except block in a function decorated with
+    `pattern_matched_exceptions`, a matching function will be called with the
+    block's expression and the current value of sys.exc_info().  If the
+    matching function returns `True`, we enter the corresponding except-block,
+    otherwise we continue to the next block, or re-raise if there are no more
+    blocks to check
+
+    Parameters
+    ----------
+    matcher : function, optional, default is `transformers.exc_patterns.match`.
+        A function accepting an expression and an exc_info tuple, returning
+        True if the exception tuple matches the expression.
+
+        The default behavior is to emulate standard python when the match
+        expression is a *subtype* of Exception, and to compare exc.type and
+        exc.args when the match expression is an *instance* of Exception.
+
+    Example
+    -------
+    >>> @pattern_matched_exceptions()
+    ... def foo():
+    ...     try:
+    ...         raise ValueError('bar')
+    ...     except ValueError('buzz'):
+    ...         return 'buzz'
+    ...     except ValueError('bar'):
+    ...         return 'bar'
+    >>> foo()
+    'bar'
+    """
+    def __init__(self, matcher=match):
+        self._matcher = matcher
+
+    def visit_COMPARE_OP(self, instr):
+        if instr.arg == Comparisons.EXCEPTION_MATCH:
+            yield ROT_TWO().steal(instr)
+            yield POP_TOP()
+            yield self.LOAD_CONST(self._matcher)
+            yield ROT_TWO()
+            yield self.LOAD_CONST(exc_info)
+            yield CALL_FUNCTION(0)
+            yield CALL_FUNCTION(2)
+        else:
+            yield instr
