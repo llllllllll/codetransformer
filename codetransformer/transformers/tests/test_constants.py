ee2a8776c4e109737dba673a5ffa446b94608246
llllllllll <joe@quantopian.com>
2015-08-02 03:58:05 -0400

    TST: Ensure that LOAD_NAME is handled correctly

--- codetransformer/transformers/tests/test_constants.py
+++ codetransformer/transformers/tests/test_constants.py
@@ -1,3 +1,5 @@
+from types import CodeType
+
 from codetransformer.code import Code
 from codetransformer.instructions import LOAD_CONST, LOAD_NAME
 from ..constants import asconstants
@@ -13,9 +15,20 @@ def test_global():
 
 
 def test_name():
-    new_code = asconstants(a=1).transform(
-        Code.from_pycode(
-            compile('class C: a', '<string>', 'exec').co_consts[0]),
-    )
-    assert LOAD_CONST(1) in new_code
-    assert LOAD_NAME('a') not in new_code
+    for const in compile(
+            'class C:\n    b = a', '<string>', 'exec').co_consts:
+
+        if isinstance(const, CodeType):
+            pre_transform = Code.from_pycode(const)
+            assert LOAD_NAME('a') in pre_transform
+            code = asconstants(a=1).transform(pre_transform)
+            break
+    else:
+        raise AssertionError('There should be a code object in there!')
+
+    assert LOAD_CONST(1) in code
+    assert LOAD_NAME('a') not in code
+
+    ns = {}
+    exec(code.to_pycode(), ns)
+    assert ns['b'] == 1
