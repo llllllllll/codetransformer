90d2f77589db1fa9d7a7987688339a1681f6f39d
llllllllll <joe@quantopian.com>
2015-10-25 23:59:17 -0400

    ENH: Adds a lazyval descriptor

--- codetransformer/utils/immutable.py
+++ codetransformer/utils/immutable.py
@@ -1,4 +1,5 @@
 from itertools import starmap
+from weakref import WeakKeyDictionary
 
 
 class immutableattr:
@@ -14,6 +15,30 @@ class immutableattr:
 
     def __get__(self, instance, owner):
         return self._attr
+
+
+class lazyval:
+    """A memoizing property.
+
+    Parameters
+    ----------
+    func : callable
+        The function used to compute the value of the descriptor.
+    """
+    def __init__(self, func):
+        self._cache = WeakKeyDictionary()
+        self._func = func
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+
+        cache = self._cache
+        try:
+            return cache[instance]
+        except KeyError:
+            cache[instance] = val = self._func(instance)
+            return val
 
 
 def _no_arg_init(self):
