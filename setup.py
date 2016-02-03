06180cb6dcb7cc08c1ba2c5193a93a33d175d0ff
Joe Jevnik <joe@quantopian.com>
2016-02-03 00:45:20 -0500

    BLD: add 3.5

--- setup.py
+++ setup.py
@@ -23,6 +23,8 @@ setup(
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Natural Language :: English',
         'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: Implementation :: CPython',
         'Operating System :: POSIX',
