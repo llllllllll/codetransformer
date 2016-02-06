34d1dd0e0b9391a93c56d21f61518729ef458232
Scott Sanderson <ssanderson@quantopian.com>
2016-02-05 19:15:24 -0500

    TEST: Add another test for precomputed_slices.

--- codetransformer/transformers/tests/test_precomputed_slices.py
+++ codetransformer/transformers/tests/test_precomputed_slices.py
@@ -14,6 +14,24 @@ def test_precomputed_slices():
     assert foo(l) == l[1:5]
     assert slice(1, 5) in foo.__code__.co_consts
 
-    instrs = Code.from_pycode(foo.__code__).instrs
+    instrs = Code.from_pyfunc(foo).instrs
     assert LOAD_CONST(slice(1, 5)).equiv(instrs[1])
     assert BUILD_SLICE not in set(map(type, instrs))
+
+
+def test_precomputed_slices_non_const():
+
+    transformer = precomputed_slices()
+
+    def f(a, b):
+        with_non_const = a[b]
+        with_mixed = a[1, b]
+        return with_non_const, with_mixed
+
+    transformed = transformer(f)
+
+    f_instrs = Code.from_pyfunc(f).instrs
+    transformed_instrs = Code.from_pyfunc(transformed).instrs
+
+    for orig, xformed in zip(f_instrs, transformed_instrs):
+        assert orig.equiv(xformed)
